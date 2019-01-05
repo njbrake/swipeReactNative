4a32657854b071b1106f0c637eec97d65432195d
Nate Brake <njbrake@gmail.com>
2019-01-05 18:38:19 -0500

    implemented swipe transition and stacking functionality

--- App.js
+++ App.js
@@ -61,10 +61,22 @@ export default class App extends React.Component {
 			</Card>
 		);
 	}
+	renderNoMoreCards() {
+		return (
+			<Card title="All Done">
+				<Text style={{ marginBottom: 10 }}> There's no more content here</Text>
+				<Button title="Get More" backgroundColor="#03a9f4" />
+			</Card>
+		);
+	}
 	render() {
 		return (
 			<View style={styles.container}>
-				<Deck data={DATA} renderCard={this.renderCard} />
+				<Deck
+					data={DATA}
+					renderCard={this.renderCard}
+					renderNoMoreCards={this.renderNoMoreCards}
+				/>
 			</View>
 		);
 	}
--- src/Deck.js
+++ src/Deck.js
@@ -5,29 +5,75 @@ import {
 	Animated,
 	PanResponder,
 	Dimensions,
+	LayoutAnimation,
+	UIManager,
 } from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
+const swipeThreshold = Dimensions.get('window').width / 4;
+const swipeOutDuration = 250;
+
 class Deck extends React.Component {
+	static defaultProps = {
+		onSwipeRight: () => {},
+		onSwipeLeft: () => {},
+		renderNoMoreCards: () => {},
+	};
 	constructor(props) {
 		super(props);
 		const position = new Animated.ValueXY();
 		const panResponder = PanResponder.create({
 			onStartShouldSetPanResponder: () => true,
 			onPanResponderMove: (e, gesture) => {
-				console.log(gesture);
 				position.setValue({ x: gesture.dx, y: gesture.dy });
 			},
-			onPanResponderRelease: () => {
-				this.resetPosition();
+			onPanResponderRelease: (e, gesture) => {
+				if (gesture.dx >= swipeThreshold) {
+					this.forceSwipeRight();
+				} else if (gesture.dx < -swipeThreshold) {
+					this.forceSwipeLeft();
+				} else {
+					this.resetPosition();
+				}
 			},
 		});
 		this.state = {
 			panResponder,
 			position,
+			index: 0,
 		};
 	}
 
+	componentWillUpdate() {
+		UIManager.setLayoutAnimationEnabledExperimental &&
+			UIMAanager.setLayoutAnimationEnabledExperimental(true);
+		LayoutAnimation.spring();
+	}
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.data !== this.props.data) {
+			this.setState({ index: 0 });
+		}
+	}
+	forceSwipeRight() {
+		Animated.timing(this.state.position, {
+			toValue: { x: screenWidth, y: 0 },
+			duration: swipeOutDuration,
+		}).start(() => this.onSwipeComplete('right'));
+	}
+	forceSwipeLeft() {
+		Animated.spring(this.state.position, {
+			toValue: { x: -screenWidth, y: 0 },
+			duration: swipeOutDuration,
+		}).start(() => this.onSwipeComplete('left'));
+	}
+
+	onSwipeComplete(direction) {
+		const { onSwipeLeft, onSwipeRight, data } = this.props;
+		const item = data[this.state.index];
+		direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+		this.state.position.setValue({ x: 0, y: 0 });
+		this.setState({ index: this.state.index + 1 });
+	}
 	resetPosition() {
 		Animated.spring(this.state.position, {
 			toValue: { x: 0, y: 0 },
@@ -46,24 +92,45 @@ class Deck extends React.Component {
 	}
 
 	renderCards() {
-		return this.props.data.map((item, index) => {
-			if (index === 0) {
+		if (this.state.index >= this.props.data.length) {
+			return this.props.renderNoMoreCards();
+		}
+		return this.props.data
+			.map((item, i) => {
+				if (i < this.state.index) {
+					return null;
+				}
+				if (i === this.state.index) {
+					return (
+						<Animated.View
+							key={i}
+							style={[this.getCardStyle(), styles.card]}
+							{...this.state.panResponder.panHandlers}
+						>
+							{this.props.renderCard(item)}
+						</Animated.View>
+					);
+				}
 				return (
 					<Animated.View
-						key={index}
-						style={this.getCardStyle()}
-						{...this.state.panResponder.panHandlers}
+						key={item.id}
+						style={[styles.card, { top: 10 * (i - this.state.index) }]}
 					>
 						{this.props.renderCard(item)}
 					</Animated.View>
 				);
-			}
-			return this.props.renderCard(item);
-		});
+			})
+			.reverse();
 	}
 	render() {
 		return <Animated.View>{this.renderCards()}</Animated.View>;
 	}
 }
 
+const styles = {
+	card: {
+		position: 'absolute',
+		width: screenWidth,
+	},
+};
 export default Deck;
